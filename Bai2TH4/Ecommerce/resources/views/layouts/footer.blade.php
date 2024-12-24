d0edfa033746d44d0ff35f699e48f7f01e526d82
Le Thi Ly <145178323+lethily2402@users.noreply.github.com>
2024-12-24 23:58:28 +0700

    Delete Bai2TH4/Ecommerce/resources/views directory

--- Bai2TH4/Ecommerce/resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <footer class="py-3">
-        <p class="text-center text-body-secondary border-top m-0 pt-3">
-            &copy; 2024 Company, Inc. All rights reserved.
-        </p>
-    </footer>
-</div>
