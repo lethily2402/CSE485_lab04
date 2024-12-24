dd5dd06d3ecb5b34fb82423f758acb85b8f75a28
elnino1308 <hoduc105@gmail.com>
2024-12-24 21:51:06 +0700

    Done

--- Bai2TH3/Ecommerce/resources/views/layouts/header.blade.php
+++ Bai2TH3/Ecommerce/resources/views/layouts/header.blade.php
@@ -1,12 +1,20 @@
 <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <span class="fs-4">Quản lý khách hàng</span>
+        <a href="/"
+            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4">eCommerce</span>
         </a>
         <ul class="nav nav-pills">
             <li class="nav-item">
-                <a href="{{ route('customers.index') }}" class="nav-link{{ request()->routeIs('customers.*') ? ' active' : '' }}">
+                <a href="{{ route('customers.index') }}"
+                    class="nav-link{{ request()->routeIs('customers.*') ? ' active' : '' }}">
                     Customers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('products.index') }}"
+                    class="nav-link{{ request()->routeIs('products.*') ? ' active' : '' }}">
+                    Products
                 </a>
             </li>
         </ul>
--- Bai2TH3/Ecommerce/routes/web.php
+++ Bai2TH3/Ecommerce/routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
 Route::resource('customers', CustomerController::class);
+Route::resource('products', ProductController::class);
